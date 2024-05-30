2a2815d4415c365139ef86d97f2a2a6b4b283c10
chandlery54 <chandlery54@gmail.com>
2024-05-29 23:12:34 -0600

    Add List of English Words

---
+++ lib/src/utilities/list_english_words.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'list_english_words.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$listEnglishWordsHash() => r'ee0b8e6010fc5c0140ae196246ba46d889b5a1e3';
+
+/// See also [listEnglishWords].
+@ProviderFor(listEnglishWords)
+final listEnglishWordsProvider = AutoDisposeProvider<List<String>>.internal(
+  listEnglishWords,
+  name: r'listEnglishWordsProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$listEnglishWordsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ListEnglishWordsRef = AutoDisposeProviderRef<List<String>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
