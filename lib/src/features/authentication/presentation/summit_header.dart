ff3aa61dbc63334966aee1932d447f7f61423565
chandlery54 <chandlery54@gmail.com>
2024-06-14 21:57:21 -0600

    Add Custom Summit Rock Icon instead of svg

--- lib/src/features/authentication/presentation/summit_header.dart
+++ lib/src/features/authentication/presentation/summit_header.dart
@@ -1,48 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:vector_graphics/vector_graphics.dart';
+import 'package:summit_rock/src/utilities/summit_icons.dart';
 
 class SummitHeader extends StatelessWidget {
   const SummitHeader({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Brightness brightness = MediaQuery.of(context).platformBrightness;
-    bool darkMode = brightness == Brightness.dark;
-    String imageName = darkMode
-        ? 'assets/images/ScoringTechLogoLight.png'
-        : 'assets/images/ScoringTechLogoDark.png';
-    return Column(
+    return const Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        const Padding(
+        Padding(
           padding: EdgeInsets.symmetric(
             horizontal: 60.0,
             vertical: 20.0,
           ),
-          // child: Image.asset(
-          //   imageName,
-          //   height: 30.0,
-          // ),
-          //       VectorGraphic(
-          //       loader: AssetBytesLoader('assets/icon_heart_blue.svg'),
-          // colorFilter: ColorFilter.mode(Color(0xFF70ABE6), BlendMode.srcIn),
-          // width: 300,
-          // height: 300,
-          // ),
-          child: VectorGraphic(
-            loader: AssetBytesLoader('assets/SummitRock-01.svg'),
-            colorFilter: ColorFilter.mode(Color(0xFF70ABE6), BlendMode.srcIn),
-            width: 60,
-            height: 60,
+          child: Icon(
+            Summit.summitrocklogo,
+            size: 180.0,
           ),
         ),
-        Text(
-          'Summit Rock',
-          style: Theme.of(context).textTheme.displayMedium?.copyWith(
-                fontWeight: FontWeight.w700,
-              ),
-          textAlign: TextAlign.center,
-        ),
+        // Text(
+        //   'Summit Rock',
+        //   style: Theme.of(context).textTheme.displayMedium?.copyWith(
+        //         fontWeight: FontWeight.w700,
+        //       ),
+        //   textAlign: TextAlign.center,
+        // ),
       ],
     );
   }
